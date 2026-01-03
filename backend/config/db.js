588a65b55d7aa8b67cba03568314fd7158afb4f5
madhwanisimran <madhwanisimran146@gmail.com>
2026-01-03 19:54:53 +0530

    Move MongoDB URI to env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  await mongoose
-    .connect(
-      `mongodb+srv://madhwanisimran146_db_user:ikonkarsatnam@cluster0.z8xg0wx.mongodb.net/food_delivery`
-    )
-    .then(() => {
-      console.log("MongoDB connected");
-    });
+  await mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("MongoDB connected");
+  });
 };
