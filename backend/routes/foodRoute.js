a0f451f3f98afdbaf676997018b123933e3808ea
madhwanisimran <madhwanisimran146@gmail.com>
2026-01-12 23:18:39 +0530

    Updated foodRoute

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -5,14 +5,21 @@ import {
   removeFood,
 } from "../controllers/foodController.js";
 import upload from "../config/multer.js";
+import authMiddleware from "../middleware/auth.js";
 import adminMiddleware from "../middleware/admin.js";
 
 const foodRouter = express.Router();
 
-foodRouter.post("/add", adminMiddleware, upload.single("image"), addFood);
+foodRouter.post(
+  "/add",
+  authMiddleware,
+  adminMiddleware,
+  upload.single("image"),
+  addFood
+);
 foodRouter.get("/list", listFood);
-foodRouter.post("/remove", adminMiddleware, removeFood);
-foodRouter.post("/delete", adminMiddleware, removeFood);
-foodRouter.delete("/:id", adminMiddleware, removeFood);
+foodRouter.post("/remove", authMiddleware, adminMiddleware, removeFood);
+foodRouter.post("/delete", authMiddleware, adminMiddleware, removeFood);
+foodRouter.delete("/:id", authMiddleware, adminMiddleware, removeFood);
 
 export default foodRouter;
